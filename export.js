81dfabbd78d2354cf4cb12a8c8730bd6e5e8420c
mogi mogi <mogimogi120501@gmail.com>
2025-09-17 16:42:34 +0530

    Create export.js

---
+++ export.js
@@ -0,0 +1,5 @@
+// export.js
+document.getElementById('exportCSVLatex').addEventListener('click', ()=>window.ss.exportCSV('latex'));
+document.getElementById('exportCSVHTML').addEventListener('click', ()=>window.ss.exportCSV('html'));
+document.getElementById('exportJSONLatex').addEventListener('click', ()=>window.ss.exportJSON('latex'));
+document.getElementById('exportJSONHTML').addEventListener('click', ()=>window.ss.exportJSON('html'));
